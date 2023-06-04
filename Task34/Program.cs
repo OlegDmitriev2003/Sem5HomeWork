0e59b370a0b4cd16580913d9184597e019a65744
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-04 13:01:27 +0300

    Fixed a minor mistake in Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,7 +2,7 @@
 int count = 0;
 int [] numbers = new int [4];
 for (int i = 0 ; i < 4; i++)
-    numbers [i] = new Random().Next(100,999);
+    numbers [i] = new Random().Next(100,1000);
 Console.WriteLine (string.Join(",", numbers));
 for (int i = 0 ; i < 4; i++)
     if (numbers[i] % 2 == 0)
