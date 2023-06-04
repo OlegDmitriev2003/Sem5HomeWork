2ffc20d0b203bba3ffb6c3d18b40223ac23031ce
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-04 13:14:43 +0300

    completed Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int sum = 0;
+int [] numbers = new int [4];
+for (int i = 0; i < 4; i++)
+    numbers [i] = new Random().Next(10,100);
+Console.WriteLine(string.Join(",", numbers));
+for (int i = 0; i < 4; i++)
+    if (i%2 == 1)
+        sum = sum + numbers[i];
+Console.WriteLine($"Сумма нечётных позиций - {sum}");
