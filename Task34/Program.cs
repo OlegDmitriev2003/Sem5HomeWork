b9796177a59ffff2ca92d4e8b77179f47f169fa1
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-04 12:55:42 +0300

    Completed Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int count = 0;
+int [] numbers = new int [4];
+for (int i = 0 ; i < 4; i++)
+    numbers [i] = new Random().Next(100,999);
+Console.WriteLine (string.Join(",", numbers));
+for (int i = 0 ; i < 4; i++)
+    if (numbers[i] % 2 == 0)
+        count++;
+Console.WriteLine($"Чётных чисел - {count}");
